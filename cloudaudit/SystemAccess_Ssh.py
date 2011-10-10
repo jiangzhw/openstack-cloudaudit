4b54f2c5cae27b2b0de0a5e8028c823bb334bd3a
Josh Daymont <joshd@securisea.com>
2011-10-10 10:14:05 -0700

    Fixes for pep8 and snake_case

--- cloudaudit/SystemAccess_Ssh.py
+++ cloudaudit/SystemAccess_Ssh.py
@@ -9,7 +9,8 @@ import libssh2
 import cmd
 import types
 
-class SystemAccess_Ssh(object):
+
+class system_access_ssh(object):
 
     """
     A controller that produces information on the Glance API versions.
@@ -17,13 +18,14 @@ class SystemAccess_Ssh(object):
 
     def __init__(self):
         self.openstackclient = None
-        self.clients = { }
-        self.osmap = { }
+        self.clients = {}
+        self.osmap = {}
 
-
-    def getAllSshSystems(self):
-#        self.openstackclient  = novaclient.OpenStackClient("joshd", "f2940ebc-4808-4846-9a47-b71ed77b6fdc", "http://127.0.0.1:8774/v1.0/")
-        self.openstackclient  = novaclient.OpenStack("joshd", "f82d944c-84f7-4a60-8f61-f0ca4b54f860", "http://127.0.0.1:8774/v1.0/")
+    def get_all_ssh_systems(self):
+        self.openstackclient = \
+            novaclient.OpenStack("joshd",
+                                 "f82d944c-84f7-4a60-8f61-f0ca4b54f860",
+                                 "http://127.0.0.1:8774/v1.0/")
 
         list = self.openstackclient.images.list()
 
@@ -38,24 +40,23 @@ class SystemAccess_Ssh(object):
         list = []
 
         for server in newlist:
-            self.testSsh(server.addresses['private'][0], "", "")
+            self.test_ssh(server.addresses['private'][0], "", "")
 
         return newlist
 
-    def testSsh(self, ip, key, password):
+    def test_ssh(self, ip, key, password):
 
-        myclient = self.getSshClient(ip, key, password)
+        myclient = self.get_ssh_client(ip, key, password)
         if myclient == None:
             return False
 
         return True
 
-
-    def getSshClient(self, ip, key, password):
+    def get_ssh_client(self, ip, key, password):
 
         if ip in self.clients:
             return self.clients[ip]
-        
+
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         # XXX TODO:  don't hardcode this
@@ -68,46 +69,40 @@ class SystemAccess_Ssh(object):
 
         return client
 
-    def runSshCommnad(self, client, command):
+    def run_ssh_commnad(self, client, command):
         o_stdin, o_stdout, o_stderr = client.exec_command(command)
 
         return o_stdin, o_stdout, o_stderr
 
-    def getOS(self, server):
-        servers = [ ]
-        retvals = { }
+    def get_os(self, server):
+        servers = []
+        retvals = {}
         if isinstance(server, types.ListType):
             for srv in server:
                 if len(srv.addresses['private']) > 0:
                     servers.append(srv.addresses['private'][0])
         else:
-            servers = [ server.addresses['private'][0] ]
+            servers = [server.addresses['private'][0]]
 
         for srv in servers:
 
             if srv in self.osmap:
-                retvals[srv] = osmap[srv]
+                retvals[srv] = self.osmap[srv]
                 continue
 
-            lines = self.getCommandOutput(srv, "uname")
+            lines = self.get_command_output(srv, "uname")
             if len(lines) > 0:
                 self.osmap[srv] = lines[0].strip()
                 retvals[srv] = lines[0].strip()
 
         return retvals
 
-    def getCommandOutput(self, ip, command):
+    def get_command_output(self, ip, command):
 
-        client = self.getSshClient(ip, "", "")
+        client = self.get_ssh_client(ip, "", "")
 
-        o_stdin, o_stdout, o_stderr = self.runSshCommnad(client, command)
+        o_stdin, o_stdout, o_stderr = self.run_ssh_commnad(client, command)
 
         lines = o_stdout.readlines()
 
         return lines
-
-
-
-
-
-
